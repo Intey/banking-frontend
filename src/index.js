add0dd7b33173e9d50c22d7766db81dba3a1611c
Intey <ziexe0@gmail.com>
2018-03-12 09:09:59 +0300

    [up] bump react-scripts; add router

--- src/index.js
+++ src/index.js
@@ -2,16 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Builder from './views/Builder'
 import registerServiceWorker from './registerServiceWorker';
 
 import { Provider } from 'react-redux'
 import store from './logics/store.js'
 import { fetchEvents } from './logics/event/actions'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+    <Provider store={store}>
+      <Router>
+        <Route path="/" component={App}>
+          <Route path="/new" component={Builder}/>
+        </Route>
+      </Router>
+    </Provider>
   ,
   document.getElementById('root'));
 
