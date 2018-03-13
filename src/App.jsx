c324c2f646c4510c19d9c0a26358bc367aa8f729
Intey <ziexe0@gmail.com>
2018-03-13 10:05:41 +0300

    [fix] correct routing epending on authorization

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import './App.css';
 import EventsList from './containers/EventsList'
 import Menu from './containers/Menu'
 import Auth from './views/Auth'
+import Builder from './views/Builder'
 import { AUTH, AUTH_ERROR } from './logics/auth/actions'
 // TODO: utils
 import { act } from './utils/action'
@@ -22,25 +24,32 @@ function mapDispatchToProps(dispatch) {
 
 const AuthContainer = connect( state => { return state }, mapDispatchToProps)(Auth)
 
-function App(props) {
-  let container
-  if (props.isloginin) {
-    container =
-      <article className="body">
-        <EventsList {...props}/>
-      </article>
-  }
-  else {
-    container = <AuthContainer/>
-  }
+
+function Header(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h3>Banking 01050-1</h3>
-        <Menu/>
-      </header>
-      {container}
-    </div>
+    <header className="app-header">
+      <h3>Banking 01050-1</h3>
+      <Menu/>
+    </header>
+  )
+}
+
+function MainHOC(authorized) {
+  return () => (authorized ?
+    <EventsList/>
+    :
+    <AuthContainer/>
+  )
+}
+function App({isloginin, fetching}) {
+  return (
+    <Router>
+      <div className="app">
+        <Header/>
+        <Route path="/" render={MainHOC(isloginin)}/>
+        <Route path="/new" component={Builder}/>
+      </div>
+    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,22 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Builder from './views/Builder'
 import registerServiceWorker from './registerServiceWorker';
 
 import { Provider } from 'react-redux'
 import store from './logics/store.js'
 import { fetchEvents } from './logics/event/actions'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 ReactDOM.render(
-    <Provider store={store}>
-      <Router>
-        <Route path="/" component={App}>
-          <Route path="/new" component={Builder}/>
-        </Route>
-      </Router>
-    </Provider>
+  <Provider store={store}>
+    <App />
+  </Provider>
   ,
   document.getElementById('root'));
 
