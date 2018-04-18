5dcf7dae16454d797c1bbf7c490d84b18a221f8c
Intey <ziexe0@gmail.com>
2018-04-18 07:57:15 +0300

    [up] show error on auth

--- src/views/Auth.jsx
+++ src/views/Auth.jsx
@@ -2,7 +2,15 @@ import React from 'react';
 import {HOST, PORT} from '../settings'
 
 export default class Auth extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      error: false,
+    }
+  }
+
   send = ()=> {
+    this.setState({error: false})
     let username = document.getElementById('username').value
     let password = document.getElementById('password').value
     fetch(`http://${HOST}:${PORT}/api/auth/`,
@@ -22,15 +30,24 @@ export default class Auth extends React.Component {
       .catch( e => {
         this.props.onFailed(e)
         console.warn(e)
+        this.setState({error: e})
       })
   }
 
   render() {
+    let errorView = (
+      this.state.error ?
+      <p className="error">{this.state.error}</p>
+      :
+      null
+    )
+
     return  (
       <form className="auth-form">
         <input id="username" type="text" name="username"/>
         <input id="password" type="password" name="password"/>
         <button type="button" onClick={this.send}>log in</button>
+        {errorView}
       </form>
     )
   }
