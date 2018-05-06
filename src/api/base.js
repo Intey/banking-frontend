655f2b9c511d96bb781e9794ba7987c5716961c3
Intey <ziexe0@gmail.com>
2018-05-06 16:44:49 +0300

    [ref] simplify api code with async/await

--- src/api/base.js
+++ src/api/base.js
@@ -2,3 +2,28 @@ import { HOST, PORT } from '../settings'
 const BASE = `http://${HOST}:${PORT}/api`
 
 export const EVENTS_URL = `${BASE}/events/`
+export const USERS_URL = `${BASE}/users/`
+
+export async function fetchErrorMiddleware(response) {
+  let res = await response
+  if (res.ok)
+    return res.json()
+  else
+    return Promise.reject(res.json())
+}
+
+export function get(URL) {
+  return fetchErrorMiddleware(fetch(URL))
+}
+
+export function post(URL, payload) {
+  return fetchErrorMiddleware(fetch(URL, {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(payload)
+  }))
+}
+
--- src/api/event.js
+++ src/api/event.js
@@ -1,27 +1,10 @@
-import { EVENTS_URL } from './base'
-
-function fetchErrorMiddleware(response) {
-  return response
-    .then ( res => {
-      if (res.ok)
-        return res.json()
-      else
-        return Promise.reject(res.json())
-    })
-}
+import { EVENTS_URL, post, get } from './base'
 
 export function createEvent(payload) {
   // console.log("create new event API", payload)
-  return fetchErrorMiddleware(fetch(EVENTS_URL, {
-    method: 'POST',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(payload)
-  }))
+  return post(EVENTS_URL, payload)
 }
 
 export function getEvents() {
-  return fetchErrorMiddleware(fetch(EVENTS_URL))
+  return get(EVENTS_URL)
 }
