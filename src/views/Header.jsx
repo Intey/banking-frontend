2984ecf36d7915225c4364fe5bed62411faa49de
Intey <ziexe0@gmail.com>
2018-06-05 09:26:49 +0300

    [up] add "go back" button in header

--- src/views/Header.jsx
+++ src/views/Header.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import './Header.css';
 import Menu from '../containers/Menu'
 import { Link } from 'react-router-dom'
+import {withRouter} from 'react-router-dom'
 
 
-export default function Header(props) {
+function Header(props) {
+  console.log(props)
   return (
     <header className="app-header">
+      <button onClick={props.history.goBack}>&lt;-</button>
       <h3><Link to='/'>Banking 01050-1</Link></h3>
       <div className="header-button">
         <Link to="/new">new</Link>
@@ -15,3 +18,6 @@ export default function Header(props) {
     </header>
   )
 }
+
+const HeaderRouted = withRouter(Header);
+export default HeaderRouted
