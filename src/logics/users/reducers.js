f2fce9c11d5f6d145e3b0896e2967f817dedb3ca
Intey <ziexe0@gmail.com>
2018-07-04 09:21:16 +0300

    [ref] namings, no class components

--- src/logics/users/reducers.js
+++ src/logics/users/reducers.js
@@ -1,12 +1,11 @@
 import { RECEIVE_USERS } from './actions'
 
-export default function USERS(state=[], action) {
+export default function users(state=[], action) {
   switch(action.type) {
     case RECEIVE_USERS:
       return action.payload
       // case CREATE_USER_RESPONSE:
       // careturn [...state, action.payload]
-
     default:
       return state
   }
--- src/views/ParticipantsList.jsx
+++ src/views/ParticipantsList.jsx
@@ -1,22 +1,14 @@
 import React from 'react'
 
-class ParticipantItem extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      ...props.account,
-    }
-  }
 
-  render() { return (
+function ParticipantItem({account}) { return (
     <li>
       <div className="participant-item">
-        <span>{this.state.username}({this.state.rate}): {this.state.parts}</span>
+        <span>{account.username}({account.rate}): {account.parts}</span>
       </div>
     </li>
-  );}
+  )
 }
-
 
 export default class ParticipantsList extends React.Component {
   constructor(props) {
