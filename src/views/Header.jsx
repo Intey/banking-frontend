20993c05c539a388b3fb27a8e623f394a636231c
Intey <ziexe0@gmail.com>
2018-07-06 20:52:20 +0300

    [fix] create new event button

--- src/views/Header.jsx
+++ src/views/Header.jsx
@@ -11,7 +11,7 @@ function Header(props) {
       <button onClick={props.history.goBack}>&lt;-</button>
       <h3><Link to='/'>Banking 01050-1</Link></h3>
       <div className="header-button">
-        <Link to="/new">new</Link>
+        <Link to="/new-event">new</Link>
       </div>
       <div className="header-button" onClick={props.onLogout}>Log out</div>
       <Menu/>
